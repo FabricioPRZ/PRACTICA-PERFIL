6c80405437673b7164bf402e857a69ccf829ff41
FabricioPRZ <233394@ids.upchiapas.edu.mx>
2026-05-20 14:37:46 -0600

    fix device preview

--- lib/app.dart
+++ lib/app.dart
@@ -1,3 +1,4 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:practica/features/profile/data/datasources/profile_local_datasource.dart';
 import 'package:practica/features/profile/data/repositories/profile_repository_impl.dart';
@@ -7,30 +8,48 @@ import 'package:practica/features/profile/presentation/pages/profile_page.dart';
 import 'package:practica/shared/theme/theme.dart';
 import 'package:practica/shared/theme/util.dart';
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final brightness = View.of(context).platformDispatcher.platformBrightness;
-    final TextTheme textTheme = createTextTheme(context, 'Poppins', 'Poppins');
-    final MaterialTheme theme = MaterialTheme(textTheme);
+  State<MyApp> createState() => _MyAppState();
+}
 
-    // Inyección manual de dependencias
-    // datasource → repository → use case → notifier → page
-    final notifier = ProfileNotifier(
+class _MyAppState extends State<MyApp> {
+  late final ProfileNotifier _notifier;
+
+  @override
+  void initState() {
+    super.initState();
+    _notifier = ProfileNotifier(
       GetProfile(
         ProfileRepositoryImpl(
           const ProfileLocalDataSourceImpl(),
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _notifier.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final brightness = MediaQuery.platformBrightnessOf(context);
+    final TextTheme textTheme = createTextTheme(context, 'Poppins', 'Poppins');
+    final MaterialTheme theme = MaterialTheme(textTheme);
 
     return MaterialApp(
       title: 'Perfil de Usuario',
       debugShowCheckedModeBanner: false,
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       theme: brightness == Brightness.light ? theme.light() : theme.dark(),
-      home: ProfilePage(notifier: notifier),
+      home: ProfilePage(notifier: _notifier),
     );
   }
 }
